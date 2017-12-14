c9168eaedbdb182b974b20b42d934b86b0e7ba88
ddsjoberg <danield.sjoberg@gmail.com>
2017-12-14 16:07:27 -0500

    added more tests for testthat

--- R/data.R
+++ R/data.R
@@ -1,24 +1,24 @@
-set.seed(5641324)
-
-# simulating 2 covariates to predict time to event
-seq.n = 15
-sjosmooth.tbl = tibble::tibble(x = seq(from = 0, to = 1, length.out = seq.n),
-                               y = seq(from = 0, to = 1, length.out = seq.n))
-sjosmooth.tbl = tidyr::complete(sjosmooth.tbl, x, y)
-
-# simulating the time to event outcome assuming a correct Cox model
-sjosmooth.tbl = dplyr::mutate(sjosmooth.tbl,
-                              xb = x + y,
-                              time.true = -log(runif(seq.n^2, 0, 1)) * exp(-xb),
-                              time.cens = runif(seq.n^2, 0, max(time.true)),
-                              time.obs = pmin(time.true, time.cens),
-                              event = as.numeric(time.true<= time.cens))
-
-# creating a data.frame version of the data
-sjosmooth.df = data.frame(sjosmooth.tbl)
-
-#appending datasets to package
-usethis::use_data(sjosmooth.tbl, sjosmooth.df, overwrite = TRUE)
+# set.seed(5641324)
+#
+# # simulating 2 covariates to predict time to event
+# seq.n = 15
+# sjosmooth.tbl = tibble::tibble(x = seq(from = 0, to = 1, length.out = seq.n),
+#                                y = seq(from = 0, to = 1, length.out = seq.n))
+# sjosmooth.tbl = tidyr::complete(sjosmooth.tbl, x, y)
+#
+# # simulating the time to event outcome assuming a correct Cox model
+# sjosmooth.tbl = dplyr::mutate(sjosmooth.tbl,
+#                               xb = x + y,
+#                               time.true = -log(runif(seq.n^2, 0, 1)) * exp(-xb),
+#                               time.cens = runif(seq.n^2, 0, max(time.true)),
+#                               time.obs = pmin(time.true, time.cens),
+#                               event = as.numeric(time.true<= time.cens))
+#
+# # creating a data.frame version of the data
+# sjosmooth.df = data.frame(sjosmooth.tbl)
+#
+# #appending datasets to package
+# usethis::use_data(sjosmooth.tbl, sjosmooth.df, overwrite = TRUE)
 
 #' A simulated tibble used to motivate examples in the \code{sjosmooth} package
 #'
@@ -33,9 +33,6 @@ usethis::use_data(sjosmooth.tbl, sjosmooth.df, overwrite = TRUE)
 #'   \item{event}{Death observed: yes/no (1/0)}
 #' }
 "sjosmooth.tbl"
-
-#appending datasets to package
-usethis::use_data(sjosmooth.tbl, sjosmooth.df, overwrite = TRUE)
 
 #' Same as sjosmooth.tbl, but as a data frame.  Primarily used for testing.
 #' A simulated data frame used to motivate examples in the \code{sjosmooth} package
--- tests/testthat/test-sm.timeto.R
+++ tests/testthat/test-sm.timeto.R
@@ -1,2 +1,22 @@
 context("test-sm.coxph.R")
 
+newdata = tibble::tibble(x = seq(0, 1, by = 0.5),
+                         time.true = 1)
+
+test_that("Simple case runs without error", {
+  expect_error(
+    sm.timeto(formula = Surv(time.true) ~ x,
+              data = sjosmooth.tbl,
+              newdata = newdata),
+    NA
+    )
+})
+
+test_that("Lambda cannot be negative", {
+  expect_error(
+    sm.timeto(formula = Surv(time.true) ~ x,
+              data = sjosmooth.tbl,
+              newdata = newdata,
+              lambda = -1)
+  )
+})
