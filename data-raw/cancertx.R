c7c4863e01b509b59e15c000cd1eae629cb71662
Daniel Sjoberg <sjobergd@mskcc.org>
2018-12-01 17:46:23 -0500

    updating documentation

--- data-raw/cancertx.R
+++ data-raw/cancertx.R
@@ -1,4 +1,5 @@
 # Results from a simulated recur data
+library(survival)
 
 set.seed(490345)
 n <- 10000
@@ -7,9 +8,13 @@ cancertx <-
     age = rnorm(n, 30, 10),
     marker = rnorm(n, 10, 2),
     xb = (1/50) * age - 0.2 * marker,
+    xb_marker = (1/50) * 30 - 0.2 * marker,
+    xb_age = (1/50) * age - 0.2 * 10,
     time = -log(runif(n, 0, 1)) * exp(-xb),
     # getting the true survival probabilites at time 1 using x
-    survt1 = exp(-exp(xb))
+    survt1 = exp(-exp(xb)),
+    survt1_marker = exp(-exp(xb_marker)),
+    survt1_age = exp(-exp(xb_age))
   )
 
 cancertx <- dplyr::select(cancertx, -xb)
@@ -29,6 +34,35 @@ sm_regression_ex1 =
     method = "coxph",
     formula = Surv(time) ~ age,
     weighting_var = "marker",
-    newdata = dplyr::data_frame(marker = seq(7, 12, 0.5))
+    newdata = dplyr::data_frame(marker = seq(5, 15, 0.5))
   )
 save(sm_regression_ex1, file = "examples/sm_regression_ex1.rda")
+
+sm_predict_ex1 =
+  sjosmooth::sm_predict(
+    data = cancertx,
+    method = "coxph",
+    formula = Surv(time) ~ marker,
+    newdata = dplyr::data_frame(marker = seq(5, 15, 0.5), time = 1),
+    type = "survival",
+    verbose = TRUE
+  )
+save(sm_predict_ex1, file = "examples/sm_predict_ex1.rda")
+
+
+sm_predict_ex2 =
+  sjosmooth::sm_predict(
+    data = cancertx,
+    method = "coxph",
+    formula = Surv(time) ~ marker + age,
+    newdata =
+      list(
+        marker = seq(7, 13, 1),
+        age = seq(20, 40, 2)
+        ) %>%
+      purrr::cross_df() %>%
+      dplyr::mutate(time = 1),
+    type = "survival",
+    verbose = TRUE
+  )
+save(sm_predict_ex2, file = "examples/sm_predict_ex2.rda")
