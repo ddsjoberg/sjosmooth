4c753536c47cd4fc4f65e59c68cf597b23507e18
Daniel Sjoberg <sjobergd@mskcc.org>
2018-12-02 18:57:06 -0500

    added sm_predict_raw unit test

--- tests/testthat/test-utils-sm_predict_raw.R
+++ tests/testthat/test-utils-sm_predict_raw.R
@@ -13,3 +13,30 @@ test_that("utils-sm_predict_raw creates output without error/warning: glm", {
   expect_warning(glm_ex, NA)
 })
 
+test_that("utils-sm_predict_raw creates output without error/warning: coxph", {
+  library(survival)
+  glm_ex <-
+    sm_predict_raw(
+      method = "coxph",
+      object = coxph(Surv(time, status) ~ age, lung),
+      newdata = lung %>% dplyr::as_data_frame() %>% dplyr::filter(dplyr::row_number() == 1),
+      type = "failure"
+    )
+
+  expect_error(glm_ex, NA)
+  expect_warning(glm_ex, NA)
+})
+
+
+test_that("utils-sm_predict_raw no error when NULL object passed", {
+  glm_null <-
+    sm_predict_raw(
+      method = "glm",
+      object = NULL,
+      newdata = mtcars %>% dplyr::as_data_frame() %>% dplyr::filter(dplyr::row_number() == 1),
+      type = "response"
+    )
+
+  expect_error(glm_null, NA)
+  expect_warning(glm_null, NA)
+})
