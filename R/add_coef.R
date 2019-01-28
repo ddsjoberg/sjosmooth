ec10cd8cdc56ba7f9822d0e7c1e22927a97d3cfc
Daniel Sjoberg <sjobergd@mskcc.org>
2019-01-28 13:19:40 -0500

    fixed attr bug

--- R/add_coef.R
+++ R/add_coef.R
@@ -21,6 +21,7 @@ add_coef <- function(x) {
   if (!"sm_regression" %in% class(x)) {
     stop("x must be of class sm_regression")
   }
+  attr_sm_regression_inputs <- attr(x, "sm_regression_inputs")
 
   # getting single variable name from RHS of formula
   pred_var <-
@@ -46,6 +47,8 @@ add_coef <- function(x) {
         ))
     )
 
+  attr(x, "sm_regression_inputs") <- attr_sm_regression_inputs
+  class(x) <- c("sm_regression", class(x))
   return(x)
 }
 
